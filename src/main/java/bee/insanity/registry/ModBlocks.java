6f7d8af89647b2905bcea8a0d758b9e2774541aa
Bee <146317619+Tricangel@users.noreply.github.com>
2026-05-19 16:25:54 +1200

    Create ModBlocks.java

---
+++ src/main/java/bee/insanity/registry/ModBlocks.java
@@ -0,0 +1,35 @@
+package bee.insanity.registry;
+
+import bee.insanity.NothingsThere;
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import org.jspecify.annotations.Nullable;
+
+import java.util.function.Function;
+
+public class ModBlocks {
+
+public static final Block TEST = register("test", Block::new, BlockBehaviour.Properties.of(), true );
+
+    private static Block register(String name, Function<BlockBehaviour.Properties, Block> blockFunction, BlockBehaviour.Properties properties, boolean itemRequired) {
+
+        ResourceKey<Block> blockKey = ResourceKey.create(Registries.BLOCK, NothingsThere.id(name));
+        Block block = blockFunction.apply(properties.setId(blockKey));
+
+        if (itemRequired) {
+            ResourceKey<Item> itemKey = ResourceKey.create(Registries.ITEM, NothingsThere.id(name));
+            BlockItem item;
+            item = new BlockItem(block, new Item.Properties().setId(itemKey));
+            Registry.register(BuiltInRegistries.ITEM, itemKey, item);
+        }
+
+        return Registry.register(BuiltInRegistries.BLOCK, blockKey, block);
+    }
+
+}
